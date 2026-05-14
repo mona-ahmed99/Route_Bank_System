c4244c7544d00916bb169bf414879883d99b59cd
basilessam <sandyessam873@gmail.com>
2026-05-14 22:26:39 +0300

    Update Bank.cpp

--- Bank/Bank.cpp
+++ Bank/Bank.cpp
@@ -1,8 +1,8 @@
 
-
 #pragma once
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 class person {
 protected:
@@ -14,17 +14,22 @@ public:
     person() {
         name = "admin";
         id = 0;
-        password = "0";
+        password = "12345678";
     }
 
     person(string name, int id, string password) {
-        this->name = name;
         this->id = id;
-        this->password = password;
+        setname(name);
+        setpassword(password);
     }
 
     void setname(string name) {
-        this->name = name;
+        if (Validation::isValidName(name)) {
+            this->name = name;
+        }
+        else {
+            cout << "Invalid Name" << endl;
+        }
     }
 
     void setid(int id) {
@@ -32,7 +37,12 @@ public:
     }
 
     void setpassword(string password) {
-        this->password = password;
+        if (Validation::isValidPassword(password)) {
+            this->password = password;
+        }
+        else {
+            cout << "Invalid password" << endl;
+        }
     }
 
     string getname() {
@@ -46,4 +56,61 @@ public:
     string getpassword() {
         return password;
     }
+};
+class client :public person {
+private:
+	double balance;
+public:
+	client() {
+		balance = 1500;
+	}
+	void setBalance(double balance) {
+		if (Validation::isValidBalance(balance)) {
+			this->balance = balance;
+		}
+		else {
+			cout << "Invalid balance. Minimum balance is 1500" << endl;
+		}
+	}
+	double getBalance() {
+		return balance;
+	}
+	void deposit(double amount) {
+		if (amount > 0) {
+			balance += amount;
+			cout << "Deposite Done Succesfully" << endl;
+		}
+		else {
+			cout << "Invalid amount" << endl;
+
+		}
+	}
+	void withdraw(double amount) {
+		if (amount > 0 && balance - amount >= 1500) {
+			balance -= amount;
+			cout << "Withdraw Done Succesfully" << endl;
+		}
+		else {
+			cout << "Invalid Withdraw Amount. Minimum Balance Must Remain 1500" << endl;
+		}
+	}
+	void transferTo(double amount, client& recipient) {
+		if (amount > 0 && balance - amount >= 1500) {
+			balance -= amount;
+			recipient.deposit(amount);
+			cout << "Transfer Done Successfully." << endl;
+		}
+		else {
+			cout << "Transfer failed. Minimum Balance Must Remain 1500." << endl;
+		}
+	}
+	void checkBalance() {
+		cout << "Current Balance: " << balance << endl;
+	}
+	void Display() {
+		cout << "ID: " << id << endl;
+		cout << "Name: " << name << endl;
+		cout << "Password: " << password << endl;
+		cout << "Balance: " << balance << endl;
+	}
 };
