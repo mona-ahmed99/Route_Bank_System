d689c33d878d106783a2055bb360481a4f2d682d
Sherif <sherif80898@outlook.com>
2026-05-16 15:22:50 +0300

    Admin

--- Bank/Admin.cpp
+++ Bank/Admin.cpp
@@ -1,1 +1,45 @@
-#include "Admin.h"
+#pragma once
+#include "Employee.h"
+class Admin : public Employee
+{
+public:
+
+    ////// Default Constructor
+
+    Admin() : Employee() {
+
+    }
+    Admin(int id, string name, string password, double salary) : Employee(id, name, password, salary) {
+
+    }
+
+    //// setter
+
+    void setName(string name)
+    {
+        if (Validation::isValidName(name))
+            this->name = name;
+        else
+            cout << "Invalid Name" << endl;
+    }
+
+    void setPassword(string password)
+    {
+        if (Validation::isValidPassword(password))
+            this->password = password;
+        else
+            cout << "Invalid Password" << endl;
+    }
+
+    void setSalary(double salary)
+    {
+        if (Validation::isValidSalary(salary))
+            this->salary = salary;
+        else
+        {
+            cout << "Invalid Salary" << endl;
+            this->salary = 5000;
+        }
+    }
+
+};
