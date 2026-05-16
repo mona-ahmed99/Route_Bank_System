f477b2ae75d0be3194ddcd57e4edd3742424ebf0
Sherif <sherif80898@outlook.com>
2026-05-16 15:53:10 +0300

    Admin after update

--- Bank/Admin.cpp
+++ Bank/Admin.cpp
@@ -1,45 +1,4 @@
 #pragma once
-#include "Employee.h"
-class Admin : public Employee
+class Admin
 {
-public:
-
-    ////// Default Constructor
-
-    Admin() : Employee() {
-
-    }
-    Admin(int id, string name, string password, double salary) : Employee(id, name, password, salary) {
-
-    }
-
-    //// setter
-
-    void setName(string name)
-    {
-        if (Validation::isValidName(name))
-            this->name = name;
-        else
-            cout << "Invalid Name" << endl;
-    }
-
-    void setPassword(string password)
-    {
-        if (Validation::isValidPassword(password))
-            this->password = password;
-        else
-            cout << "Invalid Password" << endl;
-    }
-
-    void setSalary(double salary)
-    {
-        if (Validation::isValidSalary(salary))
-            this->salary = salary;
-        else
-        {
-            cout << "Invalid Salary" << endl;
-            this->salary = 5000;
-        }
-    }
-
 };
