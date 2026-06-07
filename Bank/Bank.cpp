d2f9683e6fb6d85182a2ff07dddb645a0cedd3f1
Mona Ahmed <113481660+mona-ahmed99@users.noreply.github.com>
2026-06-07 23:50:26 +0300

    test write and read files

--- Bank/Bank.cpp
+++ Bank/Bank.cpp
@@ -1,12 +1,36 @@
 
-#include "Screen.h"
-#include"Parser.h"
+//#include "Screen.h"
+//#include"Parser.h"
+
+#include "FilesHelper.h"
 
 int main() {
 	//Screen::runApp();
-	string line = "1-Mona-12345678-5000";
+	//test parser
+	//string line = "1-Mona-12345678-5000";
 
-	Client c = Parser::parseToClient(line);
+	//Client c = Parser::parseToClient(line);
 
-	c.Display();
+	//c.Display();
+
+
+	//test write in files helper 
+	Client c;
+
+	c.setName("Mona");
+	c.setPassword("12345678");
+	c.setBalance(5000);
+
+	FilesHelper::saveClient(c);
+
+
+	//test read from files helper
+	vector<Client> clients = FilesHelper::getClients();
+
+	for (Client c : clients)
+	{
+		c.Display();
+	}
+
+	return 0;	
 }
