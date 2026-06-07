548516c371f22b0ad6bf7893b3e294bf2cb7c9c9
Mona Ahmed <113481660+mona-ahmed99@users.noreply.github.com>
2026-06-07 23:04:32 +0300

    implement parser class

--- Bank/Bank.cpp
+++ Bank/Bank.cpp
@@ -1,6 +1,12 @@
 
 #include "Screen.h"
+#include"Parser.h"
 
 int main() {
-	Screen::runApp();
+	//Screen::runApp();
+	string line = "1-Mona-12345678-5000";
+
+	Client c = Parser::parseToClient(line);
+
+	c.Display();
 }
