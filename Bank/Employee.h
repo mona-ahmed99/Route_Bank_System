6c2b0c90b64883394ff57243fc25d35698bb17e0
Asmaa-abdel-aziz <asmaabdelaziz0020@gmail.com>
2026-06-13 18:58:13 +0300

    update employee & admin

--- Bank/Employee.h
+++ Bank/Employee.h
@@ -24,9 +24,53 @@ public:
 	}
 	//display employee info
 	void Display() {
-		cout<<"Employee Information:"<<endl;
+		cout << "Employee Information:" << endl;
 		Person::Display();
 		cout << "Salary: " << salary << endl;
 	}
-};
 
+	//add client
+	void addClient(Client& client) {
+		FilesHelper::saveClient(client);
+	}
+	//search client
+	Client* searchClient(int id) {
+		static vector<Client> clients;
+		clients = FilesHelper::getClients();
+		for (int i = 0; i < clients.size(); i++) {
+			if (clients[i].getId() == id) {
+				return &clients[i];
+			}
+		}
+		return nullptr;
+	}
+	//list client
+	void listClient() {
+		vector<Client> clients;
+		clients = FilesHelper::getClients();
+		for (int i = 0; i < clients.size(); i++) {
+			clients[i].Display();
+			cout << endl;
+		}
+	}
+	//edit client
+	void editClient(int id, string name, string password, double balance) {
+		vector<Client> clients;
+		clients = FilesHelper::getClients();
+		for (int i = 0; i < clients.size(); i++) {
+			if (clients[i].getId() == id) {
+				clients[i].setName(name);
+				clients[i].setPassword(password);
+				clients[i].setBalance(balance);
+			}
+		}
+		ofstream file("Clients.txt");
+		for (int i = 0; i < clients.size(); i++) {
+			file << clients[i].getId() << "-" 
+				 << clients[i].getName() << "-"
+				 << clients[i].getPassword() << "-"
+				 << clients[i].getBalance() << endl;
+		}
+		file.close();
+	}
+};
